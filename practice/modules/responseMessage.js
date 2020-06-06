02b518e96c04722ae9e66f1efee45c7f5cc370eb
Choong <cndqjacndqja@naver.com>
2020-06-07 02:25:40 +0900

    modules 추가

---
+++ practice/modules/responseMessage.js
@@ -0,0 +1,29 @@
+module.exports = {
+  NULL_VALUE: "필요한 값이 없습니다",
+  OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
+
+  // 회원가입
+  CREATED_USER: "회원 가입 성공",
+  DELETE_USER: "회원 탈퇴 성공",
+  ALREADY_ID: "이미 사용중인 아이디입니다.",
+
+  // 로그인
+  LOGIN_SUCCESS: "로그인 성공",
+  LOGIN_FAIL: "로그인 실패",
+  NO_USER: "존재하지 않는 회원입니다.",
+  MISS_MATCH_PW: "비밀번호가 맞지 않습니다.",
+
+  // 프로필 조회
+  READ_PROFILE_SUCCESS: "프로필 조회 성공",
+
+  // 비밀번호 암호화 level2
+  PASSWORD_CRYPTO_FAIL: "비밀번호를 저장할 수 없습니다.",
+
+  //POST
+  CREATED_POST: "게시글 생성됨",
+  READ_USER_ID: "아이디 조회 성공",
+  READ_ALL_POSTS: "전체 글 조회 성공",
+  CHANGED_USER_INFO: "글 수정 성공",
+  NULL_POST: "게시글이 없습니다",
+  DELETE_SUCCESS: "게시글 삭제 성공",
+};
